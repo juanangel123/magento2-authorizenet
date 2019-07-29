43c03414c277f8620837d16077b16a664fb51209
Juan Ángel Espigares Marín <juanangel.espigares@imaginanet.com>
2019-07-29 19:50:17 +0200

    Created Payment Method Renderer

---
+++ view/frontend/web/js/view/payment/method-renderer/cc-form.js
@@ -0,0 +1,16 @@
+define([
+   'Magento_Payment/js/view/payment/cc-form'
+],  function (Component) {
+    'use strict';
+
+    return Component.extend({
+       defaults: {
+           template: 'Pronko_Authorizenet/payment/cc-form',
+           code: 'pronko_authorizenet'
+       },
+
+        getCode: function () {
+            return this.code;
+        }
+    });
+});
